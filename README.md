6e4393b513b030dc81f23ef8062cdee5f4073a9e
Daniel Farias <danielberto05@gmail.com>
2021-10-04 07:47:13 -0300

    Construção do backend

---
+++ README.md
@@ -0,0 +1,3 @@
+# Currency Visualization
+
+Aplicação cujo objetivo é mostrar a cotação do 'Real' em relação em ralação as outras moedas.
---
+++ backend/br_med/currency/views.py
@@ -0,0 +1,14 @@
+import requests
+from django.http import JsonResponse
+
+
+def currency_api(request, date_url): #recebe o request da url e as variaveis p/ adicionar ao link da api
+
+
+        response = requests.get(
+            'https://api.vatcomply.com/rates?date='+date_url+'&base=BRL'
+            )
+
+        json = response.json()
+
+        return JsonResponse(json) #retorna o json no endpoint
